05ba92b211fa0ea3ed77b2f50c4c1523ff4b55d5
connorblake1 <70154703+connorblake1@users.noreply.github.com>
2026-01-22 02:47:51 +0000

    Deploying to gh-pages from @ connorblake1/connorblake1.github.io@7f0e11d9dfba9f05ed9577c056ea6615e9c06ef6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-repositories",
           title: "repositories",
           description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
@@ -37,42 +30,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Course materials, schedules, and resources for classes taught.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
@@ -440,8 +398,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-named-a-barry-goldwater-scholar-for-2025-one-of-441-selected-nationally-from-over-5-000-applicants",
-          title: 'Named a Barry Goldwater Scholar for 2025, one of 441 selected nationally from...',
+            },},{id: "news-named-a-stamps-scholar-for-2024-one-of-365-selected-nationally-from-over-500-000-applicants",
+          title: 'Named a Stamps Scholar for 2024, one of 365 selected nationally from over...',
           description: "",
           section: "News",},{id: "news-paper-published-in-npj-computational-materials-a-self-driving-physical-vapor-deposition-system-making-sample-specific-decisions-on-the-fly",
           title: 'Paper published in npj Computational Materials: A self-driving physical vapor deposition system making...',
